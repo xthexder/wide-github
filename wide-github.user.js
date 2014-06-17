b052fbac3e8ad5728ae1cfaa38ef21fc2b0ee4c1
User <user@tinker.localdomain>
2014-06-17 03:01:39 -0600

    Some things that I learned about and missed yesterday
    * Version bump
    * Point license in manifest.json to your LICENSE instead of the generic one
    * Correct user.js `@name` to match your manifest.json

--- wide-github.user.js
+++ wide-github.user.js
@@ -4,13 +4,13 @@
 // ==UserScript==
 // @name        Wide Github
 // @namespace   https://github.com/xthexder/wide-github
-// @description This plugin changes all github repository pages to be full width and dynamically sized.
+// @description Change all github repository pages to be full width and dynamically sized.
 // @author      xthexder
 // @copyright   2013+, xthexder (https://github.com/xthexder)
 // @contributor Jason Frey (https://github.com/Fryguy)
 // @contributor Marti Martz (https://github.com/Martii)
 // @license     MIT License; http://opensource.org/licenses/mit-license.php
-// @version     1.0.8
+// @version     1.0.9
 // @icon        https://raw.githubusercontent.com/xthexder/wide-github/master/icon.png
 // @homepageURL https://github.com/xthexder/wide-github
 // @supportURL  https://github.com/xthexder/wide-github/issues
