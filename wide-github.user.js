1793b59afbe9a99a34e7c63ac58e9399421f6f75
Jacob Wirth <xthexder@live.com>
2014-10-29 17:23:14 -0400

    Remove lots of no-longer-necessary changes Fix diff commenting width Fix split-diff width conflicts

--- wide-github.user.js
+++ wide-github.user.js
@@ -44,27 +44,24 @@
         "margin-left:-190px;" +
       "}" +
 
-      // Repository Issues
-      ".repository-content .discussion-timeline-cols {" + // New Issue form
-        "margin-left:200px;" +
+      // Repository sidebar (with .split-diff)
+      ".split-diff .repository-with-sidebar {" +
+        "padding-right:0px !important;" +
       "}" +
-      ".repository-content .discussion-timeline-cols .column.main {" +
-        "margin-left:-200px !important;" +
-        "margin-right:auto !important;" +
+      ".split-diff .repository-with-sidebar .repository-sidebar {" +
+        "margin-right:0px !important;" +
+      "}" +
+
+      // Repository Issues
+      ".repository-content .discussion-timeline {" + // Issue body
+        "margin-right:-160px;" +
+        "padding-right:160px;" +
         "width:100% !important;" +
       "}" +
-      ".repository-content .discussion-timeline {" + // Issue body
-        "margin-right:160px;" +
-        "width:auto !important;" +
-        "float: none !important;" +
+      ".repository-content .discussion-timeline .timeline-new-comment {" + // New Issue / issue comment form
+        "max-width:100% !important;" +
       "}" +
-      ".repository-content .discussion-item-body .meta:after {" + // Diff / code comments
-        "clear:left !important;" +
-      "}" +
-      ".repository-content .discussion-timeline-actions .timeline-new-comment {" +
-        "max-width:none !important;" +
-      "}" +
-      ".repository-content .inline-comments .comment-holder {" +
+      ".repository-content .inline-comments .comment-holder {" + // Diff / code comments
         "max-width:none !important;" +
       "}" +
       ".repository-content .inline-comments .inline-comment-form-container {" +
@@ -74,43 +71,12 @@
         "max-width:none !important;" +
       "}" +
 
-      // New Release form
-      ".repository-content .new-release {" +
-        "margin-left:270px;" +
-      "}" +
-      ".repository-content .new-release .column.main {" +
-        "margin-left:-270px !important;" +
-        "margin-right:auto !important;" +
-        "width:100% !important;" +
-      "}" +
-
-      // Repository settings tabs
-      ".repository-content #repo-settings {" +
-        "margin-right:235px;" +
-      "}" +
-      ".repository-content .repo-settings-content {" +
-        "margin-right:-235px !important;" +
-        "margin-left:auto !important;" +
-        "width:100% !important;" +
-      "}" +
-
       // Repository pulse page
-      ".repository-content .overall-summary .graphs {" + // Overview PR / Issue graphs
+      ".repository-content .boxed-group-inner .summary-stats {" + // Overview PR / Issue counts
         "display:table;" +
         "width:100%;" +
       "}" +
-      ".repository-content .overall-summary .graphs .graph {" +
-        "border-left:1px solid #EEEEEE;" +
-        "width:50% !important;" +
-      "}" +
-      ".repository-content .overall-summary .graphs .graph:first-child {" +
-        "border-right:none !important;" +
-      "}" +
-      ".repository-content .overall-summary .summary-stats {" + // Overview PR / Issue counts
-        "display:table;" +
-        "width:100%;" +
-      "}" +
-      ".repository-content .overall-summary .summary-stats li {" +
+      ".repository-content .boxed-group-inner .summary-stats li {" +
         "width:25% !important;" +
       "}" +
       ".repository-content .authors-and-code .section {" + // Contributors bar graph
@@ -124,23 +90,12 @@
       ".repository-content .capped-card {" + // Graph cards on contributors / graph list
         "margin:10px 10px 0 0 !important;" +
       "}" +
-        ".repository-content .traffic-graph-stats:after {" + // Traffic view / visitor count
-        "content:' ';" +
+      ".repository-content .traffic-graph-stats .summary-stats {" + // Traffic view / visitor count
         "display:table;" +
-        "clear:both;" +
+        "width:100%;" +
       "}" +
-      ".repository-content .traffic-graph-stats .col {" +
-        "display:block;" +
-        "float:left;" +
-        "width:50%;" +
-      "}" +
-      ".repository-content #top-lists .col {" + // Traffic top domains / content
-        "display:block;" +
-        "float:left;" +
-        "width:50%;" +
-      "}" +
-      ".repository-content #top-lists .col .capped-box {" +
-        "width:100% !important;" +
+      ".repository-content .traffic-graph-stats .summary-stats li {" +
+        "width:50% !important;" +
       "}" +
       "";
     (document.head || document.documentElement).appendChild(s);
