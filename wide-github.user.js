d9a79559327621920f601c3a4ffd77adf63efabd
Paul Joey Clark <joeytwiddle@gmail.com>
2018-05-16 11:36:20 +0800

    Fix the vertical line that runs through PR commits

--- wide-github.user.js
+++ wide-github.user.js
@@ -8,8 +8,9 @@
 // @copyright   2013+, xthexder (https://github.com/xthexder)
 // @contributor Jason Frey (https://github.com/Fryguy)
 // @contributor Marti Martz (https://github.com/Martii)
+// @contributor Paul "Joey" Clark (https://github.com/joeytwiddle)
 // @license     MIT; https://raw.githubusercontent.com/xthexder/wide-github/master/LICENSE
-// @version     1.2.0
+// @version     1.2.1
 // @icon        https://raw.githubusercontent.com/xthexder/wide-github/master/icon.png
 // @homepageURL https://github.com/xthexder/wide-github
 // @supportURL  https://github.com/xthexder/wide-github/issues
@@ -45,6 +46,9 @@ var styleSheet = "" +
   "margin-left: -220px;" +
   "padding-left: 220px;" +
   "width: 100% !important;" +
+"}" +
+".discussion-timeline::before {" + // The vertical line running through the commit list on PRs
+  "margin-left: 220px;" +
 "}" +
 ".repository-content .discussion-sidebar {" +
   "width: 200px !important;" +
