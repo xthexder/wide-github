9274f82e57687bfac55f83e448285aeacabc4184
Jacob Wirth <xthexder@users.noreply.github.com>
2022-07-14 15:24:21 -0400

    Update to manifest version 3

--- background.js
+++ background.js
@@ -1,14 +1,13 @@
 var toggle = true;
 
-chrome.browserAction.onClicked.addListener(function(tab){
+chrome.action.onClicked.addListener((tab) => {
   toggle = !toggle;
-  chrome.browserAction.setIcon({path: (toggle ? "icons/icon.png" : "icons/icon-disabled.png")});
+  chrome.action.setIcon({path: (toggle ? "icons/icon.png" : "icons/icon-disabled.png")});
   chrome.tabs.sendMessage(tab.id, {enableWideGithub: toggle});
 });
 
-chrome.runtime.onMessage.addListener(function(request, sender, callback) {
-  if (request.reload)
-  {
+chrome.runtime.onMessage.addListener((request, sender, callback) => {
+  if (request.reload) {
     callback({enableWideGithub: toggle});
   }
 });
