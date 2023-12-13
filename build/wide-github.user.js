9a82b9ab945123462e9c8728c39c3c1fd00ae836
Jacob Wirth <xthexder@users.noreply.github.com>
2023-12-12 17:09:09 -0800

    Bump version number

--- build/wide-github.user.js
+++ build/wide-github.user.js
@@ -12,7 +12,7 @@
 // @contributor Robert Laverty (https://github.com/roblav96)
 // @contributor Amir Meimari (https://github.com/amirmeimari)
 // @license     MIT; https://raw.githubusercontent.com/xthexder/wide-github/master/LICENSE
-// @version     1.5.2
+// @version     1.6.0
 // @icon        https://raw.githubusercontent.com/xthexder/wide-github/master/icons/icon.png
 // @homepageURL https://github.com/xthexder/wide-github
 // @supportURL  https://github.com/xthexder/wide-github/issues
