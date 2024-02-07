32a42a878d1aeb3f2aacf5b2439e5649d3926d38
Jacob Wirth <xthexder@users.noreply.github.com>
2024-02-07 14:59:54 -0800

    Add new CSS selector to fix some JS-based page loads

--- build/wide-github.user.js
+++ build/wide-github.user.js
@@ -12,7 +12,7 @@
 // @contributor Robert Laverty (https://github.com/roblav96)
 // @contributor Amir Meimari (https://github.com/amirmeimari)
 // @license     MIT; https://raw.githubusercontent.com/xthexder/wide-github/master/LICENSE
-// @version     1.6.4
+// @version     1.6.5
 // @icon        https://raw.githubusercontent.com/xthexder/wide-github/master/icons/icon.png
 // @homepageURL https://github.com/xthexder/wide-github
 // @supportURL  https://github.com/xthexder/wide-github/issues
@@ -35,6 +35,10 @@ var styleSheet = "" +
   "margin-right: 0;" +
 "}" +
 "body:not(.wgh-disabled) .Layout-main div[style=\"--sticky-pane-height: 100vh;\"] > div[class^='Box-sc-'] > div[class^='Box-sc-'] > div[class^='Box-sc-'] > div[class^='Box-sc-']:nth-child(2) {" +
+  "max-width: none;" +
+"}" +
+// Alternative match for above to handle JS-loaded vs full refresh pages
+"body:not(.wgh-disabled) .Layout-main div[style=\"--sticky-pane-height:100vh\"] > div[class^='Box-sc-'] > div[class^='Box-sc-'] > div[class^='Box-sc-'] > div[class^='Box-sc-']:nth-child(2) {" +
   "max-width: none;" +
 "}" +
 "body:not(.wgh-disabled) .Layout-main div[data-selector=\"repos-split-pane-content\"] {" +
